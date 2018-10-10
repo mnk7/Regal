c68ed39be077eb3a61dbd47fab2b0b1df7394a76
fhaneder <fhaneder@gmx.net>
2018-10-10 13:57:04 +0200

    QStrings in std::strings umgewandelt

--- buch.h
+++ buch.h
@@ -1,6 +1,6 @@
 #ifndef BUCH_H
 #define BUCH_H
-#include <QString>
+#include <string>
 
 class Buch
 {
@@ -8,46 +8,46 @@ public:
     enum GENRE {Sci-Fi, Fantasy, Literary Fiction, Thriller, Politics, Philosophy, Biography};
     enum LANGUAGE {English, German}
     Buch()=default;
-    Buch(QString h): handle(h){}
-    Buch(QString& h, QString& t, QString& s, QString& a, GENRE& g, unsigned int& p, unsigned int& sr, unsigned int& er, float& r, LANGUAGE& l):
+    Buch(std::string h): handle(h){}
+    Buch(std::string& h, std::string& t, std::string& s, std::string& a, GENRE& g, unsigned int& p, unsigned int& sr, unsigned int& er, float& r, LANGUAGE& l):
         handle(h), title(t), subtitle(s), author(a), pagecount(p), startRead(sr), endRead(er), rating(r){
         genre=g;
         language=l;
     }
-    QString& handle() const;
-    QString& title() const;
-    QString& subtitle() const;
-    QString& author() const;
+    std::string& handle() const;
+    std::string& title() const;
+    std::string& subtitle() const;
+    std::string& author() const;
     GENRE& genre() const;
     unsigned int& pagecount() const;
     unsigned int& startRead() const;
     unsigned int& endRead() const;
     float& rating() const;
     LANGUAGE& language() const;
-    QString& notes() const;
-    void setTitle(QString const&);
-    void setSubtitle(QString const&);
-    void setAuthor(QString const&);
+    std::string& notes() const;
+    void setTitle(std::string const&);
+    void setSubtitle(std::string const&);
+    void setAuthor(std::string const&);
     void setGenre(GENRE const&);
     void setPagecount(unsigned int const&);
     void setStart(unsigned int const&);
     void setEnd(unsigned int const&);
     void setRating(float const&);
     void setLanguage(LANGUAGE const&);
-    void setNotes(QString const&);
+    void setNotes(std::string const&);
 
 private:
-    QString handle="";
-    QString title="";
-    QString subtitle="";
-    QString author="";
+    std::string handle="";
+    std::string title="";
+    std::string subtitle="";
+    std::string author="";
     GENRE genre=Politics;
     unsigned int pagecount=0;
     unsigned int startRead=0;
     unsigned int endRead=0;
     float rating=0;
     LANGUAGE language=English;
-    QString notes="";
+    std::string notes="";
 
 };
 
