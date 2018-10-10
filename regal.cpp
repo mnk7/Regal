f84533f7f8a20f86e2b68871296f02b381e6b111
Michael Nunhofer <MichaelNunhofer@t-online.de>
2018-10-10 16:22:08 +0200

    Deal with no input file at startup

--- regal.cpp
+++ regal.cpp
@@ -6,10 +6,10 @@ Regal::Regal(QWidget *parent)
     : QWidget(parent)
 {
     path = QFileDialog::getOpenFileName(this, "Öffne Datenbank", "/User/mnk7/Documents/workspaces/Qt-Kurs/Regal");
-    datenbank = loadData(path.toStdString());
-
-    for(const Buch& b: datenbank) {
-        printBuch(std::cout, b);
+    if(path.size() > 0) {
+        datenbank = loadData(path.toStdString());
+    } else {
+        exit(2);
     }
 }
 
