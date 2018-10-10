786cebf6e4a2371709567b6359b5478fa1cc8736
fhaneder <fhaneder@gmx.net>
2018-10-10 13:25:48 +0200

    Data für Buch

--- buch.h
+++ buch.h
@@ -5,10 +5,22 @@
 class Buch
 {
 public:
+    enum GENRE {Sci-Fi, Fantasy, Literary Fiction, Thriller, Politics, Philosophy, Biography};
+    enum LANGUAGE {English, German}
     Buch();
 private:
     QString handle;
     QString title;
+    QString subtitle;
+    QString author;
+    GENRE genre;
+    unsigned int pagecount;
+    unsigned int startRead;
+    unsigned int endRead;
+    float rating;
+    LANGUAGE language;
+    QString notes;
+
 };
 
 #endif // BUCH_H
