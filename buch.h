304bc998dc1adbc80a7720e683ef580d67db1802
fhaneder <fhaneder@gmx.net>
2018-10-10 13:45:09 +0200

    buch.h Funktionen deklariert

--- buch.h
+++ buch.h
@@ -8,8 +8,29 @@ public:
     enum GENRE {Sci-Fi, Fantasy, Literary Fiction, Thriller, Politics, Philosophy, Biography};
     enum LANGUAGE {English, German}
     Buch();
+    QString& title() const;
+    QString& subtitle() const;
+    QString& author() const;
+    GENRE& genre() const;
+    unsigned int& pagecount() const;
+    unsigned int& startRead() const;
+    unsigned int& endRead() const;
+    float& rating() const;
+    LANGUAGE& language() const;
+    QString& notes() const;
+    void setTitle(QString const&);
+    void setSubtitle(QString const&);
+    void setAuthor(QString const&);
+    void setGenre(GENRE const&);
+    void setPagecount(unsigned int const&);
+    void setStart(unsigned int const&);
+    void setEnd(unsigned int const&);
+    void setRating(float const&);
+    void setLanguage(LANGUAGE const&);
+    void setNotes(QString const&);
+
 private:
-    QString handle;
+    QString const handle;
     QString title;
     QString subtitle;
     QString author;
