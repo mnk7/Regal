ec4ddd25e3c3961fbf5e4e367b468e18c34fbf98
Michael Nunhofer <MichaelNunhofer@t-online.de>
2018-10-10 14:31:48 +0200

    small changes

--- helfer.cpp
+++ helfer.cpp
@@ -12,28 +12,30 @@ std::vector<Buch> loadData(const std::string path) {
         input.erase(std::remove(input.begin(), input.end(), '\t'), input.end());
         //find starting point of book entry
         if(auto pos = input.find('{') != input.npos) {
-            data.push_back(new Buch(input.substr(0, pos - 1)));
+            data.push_back(Buch(input.substr(0, pos - 1)));
         }
         //find equal sign
         if(auto pos = input.find('=') != input.npos) {
             std::string label = input.substr(0, pos - 1);
+            std::string value = input.substr(pos + 1, input.size());
             //remove spaces
             label.erase(remove_if(label.begin(), label.end(), isspace),
                          label.end());
-            std::string value = input.substr(pos + 1, input.size());
+
+            Buch &b = data[data.size() - 1];
 
             if(label.compare("title")) {
-
+                b.setTitle(value);
             } else if(label.compare("subtitle")) {
-
+                b.setSubtitle(value);
             } else if(label.compare("author")) {
-
+                b.setAuthor(value);
             } else if(label.compare("genre")) {
 
             } else if(label.compare("pagecount")) {
-
-            } else if(label.compare("read")) {
-
+                b.setPagecount(static_cast<unsigned int>(std::stoul(value)));
+            } else if(label.compare("startRead")) {
+                b.setStart(static_cast<unsigned int>(std::stoul(value)));
             } else if(label.compare("rating")) {
 
             } else if(label.compare("language")) {
