b8f6acffcf5b7a9e64f8267b839ffe6c526f575f
Michael Nunhofer <MichaelNunhofer@t-online.de>
2018-10-12 10:27:00 +0200

    Remove comments

--- stapel.cpp
+++ stapel.cpp
@@ -11,8 +11,6 @@ Stapel::Stapel(QWidget *parent, std::vector<Buch> &d) : QWidget(parent)
     for(decltype (datenbank->size()) i = 0; i < datenbank->size(); ++i) {
         //the first button is related to the first book etc.
         buecher[i] = new StapelElement(this, datenbank->at(i));
-        //buecher[i]->setGeometry(0, 0,
-        //                        buecher[i]->width(), buecher[i]->height());
         this->layout()->addWidget(buecher[i]);
     }
 
